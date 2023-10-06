6d80b8ca22f6a6fcc5e25728392f0cee7d4b6056
Roshan Sah <118677669+roshansah21@users.noreply.github.com>
2023-10-07 00:38:10 +0545

    Add files via upload

---
+++ day8.js
@@ -0,0 +1,34 @@
+// Comparison of datatypes in javascript
+
+/*
+
+console.log(2>1)
+console.log(2>=1)
+console.log(2<1)
+console.log(2==1)
+console.log(2 != 1)
+
+*/
+
+
+console.log("2"  >  1);
+console.log("02" >  1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined > 0);
+
+/*
+The reason is that an equility check == and comaparisons > < >= <= work differently.
+Comparisons convert null to a number, treating it as 0.
+That's why (3) null >=0 is true and (1) null >0 is false.
+*/
+
+
+
+
+
