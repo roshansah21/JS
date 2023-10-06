58f562bcb9ea2d20e71bb82ec43e3ac77a3670b3
Roshan Sah <118677669+roshansah21@users.noreply.github.com>
2023-10-07 00:37:47 +0545

    Add files via upload

---
+++ day7.js
@@ -0,0 +1,57 @@
+// Why string to number conversion is confusing
+
+/*                            Operation                                     */
+
+let value = 3
+let negValue = -value
+//console.log(negValue)
+/*
+
+console.log(3+32)
+console.log(3-32)
+console.log(3/32)
+console.log(3*32)
+console.log(3**32)
+console.log(2%3)
+
+*/
+
+let str1 = "hello"
+let str2 = " roshan"
+let str3 = str1 + str2
+//console.log(str3)
+
+/*
+// Tricks
+console.log("1" + 2) //-->first taking string and then adding
+console.log(2 + "1")
+console.log("1" + 2 + 2)
+console.log(2+2+"1")
+
+
+
+console.log(+true)
+console.log(+"")
+
+*/
+
+let num1,num2,num3
+num1 = num2 = num3 = 2 + 2 
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter)
+
+//prefix and postfix operators study please
+
+
+
+
+
+
+
+
+
+
+
+
