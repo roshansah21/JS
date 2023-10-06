7afe390907328bf7d1f123bc84e8f095991cf1e3
roshansah21 <10532sahroshan@gmail.com>
2023-10-06 11:35:59 +0545

    commit-js

---
+++ script.js
@@ -0,0 +1,31 @@
+
+/* console.log("Hello Roshan")*/
+const accountId = 12545
+let accountEmail = "Example@gmail.com"
+var accountPassword = "12354"
+accountCity = "Janakpur"
+let accountState;  //---> underfined
+
+/* accountId = 4  constant will never change */
+
+console.log(accountId)
+
+/*
+prefer not to use var    --- >{}
+because of issue of block scope and functional scope
+
+
+*/
+
+
+
+
+accountEmail = "exa@gmail.com"
+accountPassword = "544564"
+accountCity = "Kathmandu"
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountState])  // Table format
+
+
+
+//--------------------------------------------------------------------------------------
