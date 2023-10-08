ce064df9649c287ce62e40c87c3d836c64076eab
Roshan Sah <118677669+roshansah21@users.noreply.github.com>
2023-10-08 09:58:36 +0545

    Add files via upload

---
+++ day12.js
@@ -0,0 +1,34 @@
+// Number and Maths in JavaScript
+
+// const score = 560
+// console.log(score)
+//  const balance = new Number(560)
+//  console.log(balance)
+
+//  console.log(balance.toString())
+//  console.log(balance.toString().length)
+//  console.log(balance.toFixed(1))
+
+//  const otherNumber = 6.8
+//  console.log(otherNumber.toPrecision())
+
+
+
+//                  MATHS
+
+
+ console.log(Math)
+ console.log(Math.abs(4))
+ console.log(Math.round(4.6))
+ console.log(Math.floor(4.2))
+ console.log(Math.ceil(5.6))
+ console.log(Math.min(4,6,1))
+ console.log(Math.max(12,8,5,66))
+ console.log(Math.random());
+ console.log((Math.random()*10) + 1);
+ console.log(Math.floor(Math.random()*10) + 1);
+
+ const min = 10;
+ const max = 20;
+
+ console.log(Math.floor(Math.random() * ( min - max + 1)) + min)
