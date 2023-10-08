da3390ba20f3fae91e6109152851442950e80f93
Roshan Sah <118677669+roshansah21@users.noreply.github.com>
2023-10-08 09:58:12 +0545

    Add files via upload

---
+++ day11.js
@@ -0,0 +1,37 @@
+// String in javascript
+
+// const name = "Roshan"
+// const full_name = "sah"
+// const pura_naam = name + full_name;
+// // console.log(pura_naam + " 22") --> Old technique 
+
+// // console.log(`Hello my name is  ${name} and  ${full_name}`);
+
+// const gameName = new String("garina free fire")
+// console.log(gameName.toUpperCase())
+// console.log(gameName.length)
+// console.log(gameName.match)
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('n'))
+
+
+// // const newString=gameName.substring(0,5)
+// // console.log(newString)
+
+// const anotherString = gameName.slice(-2,2)
+// console.log(anotherString);
+
+
+const trimstring = "   Roshan "
+console.log(trimstring);
+console.log(trimstring.trim());
+
+
+// Replacing string useful
+const url = "https://roshan%20sah"
+console.log(url.replace('%20','_'));
+
+console.log(url.includes('Roshan'))
+
+
+// study through console in chrome for string
