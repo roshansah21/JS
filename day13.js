760409bd757166f2463326e22587b51f2263e585
Roshan Sah <118677669+roshansah21@users.noreply.github.com>
2023-10-09 08:24:32 +0545

    Add files via upload

---
+++ day13.js
@@ -0,0 +1,44 @@
+// Date and Time in depth
+/*
+
+let myDate = new Date()
+console.log(myDate)
+console.log(myDate.toString())
+console.log(myDate.toJSON())
+console.log(myDate.toISOString())
+console.log(myDate.toLocaleString())
+
+console.log(typeof myDate)//--> date is an object in js
+
+
+
+//let myCurrentDate = new Date(2030,7,26);
+let myCurrentDate = new Date(2030,7,26,14,15);
+///console.log(myCurrentDate.toLocaleString())
+
+let myCurrentDate = new Date("01-26-2030");  //--> it will give jan 26 2030
+
+
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp)
+console.log(myCurrentDate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+*/
+
+let newDate = new Date()
+console.log(newDate)
+console.log(newDate.getMonth())
+console.log(newDate.getDay())
+console.log(newDate.getTime())
+
+
+
+
+
+
+
+
+
+
