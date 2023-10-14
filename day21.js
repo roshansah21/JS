80c414429b2b8e49e191dcc0b61770ea03e61e71
Roshan Sah <118677669+roshansah21@users.noreply.github.com>
2023-10-15 00:05:37 +0545

    Add files via upload

---
+++ day21.js
@@ -0,0 +1,24 @@
+// Global and Local scope in js
+/*
+let firstName = 1
+const secondName = 2
+var thirdName = 3
+*/
+
+   // In object also this come,but if {} come of with any function or else/if
+
+if (true){
+let firstName = 1
+const secondName = 2
+var thirdName = 3
+}
+
+
+//console.log(firstName);
+//console.log(secondName); 
+console.log(thirdName);
+
+for (let i = 0; i < array.length; i++) {
+   const element = array[i];
+   
+}
