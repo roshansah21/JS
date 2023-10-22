ec5139a6bcd17de912b51af36d7180f0911371bf
Roshan Sah <118677669+roshansah21@users.noreply.github.com>
2023-10-22 09:27:04 +0545

    Add files via upload

---
+++ day28.js
@@ -0,0 +1,25 @@
+// while and do while loop in js
+
+let index = 0
+while (index <= 10){
+    //console.log(`value of index is ${index}`);
+    index = index + 2;
+}
+
+let myArray = ['ironman','thor','wonda']
+let arr = 0
+while (arr < myArray.length) {
+   // console.log(`value is ${myArray[arr]}`);
+    arr = arr + 1
+    
+}
+
+
+
+// Do while loop
+
+let mark = 1
+do {
+    console.log(`mark is ${mark}`);
+    mark++
+} while (mark <= 10);
